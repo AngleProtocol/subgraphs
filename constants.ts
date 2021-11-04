25dcd362237a96184fedcc08734ada2c8594c054
Guillaume Nervo <guillaume.nervo@gmail.com>
2021-11-04 11:58:00 +0100

    update historical data every 15mins

--- constants.ts
+++ constants.ts
@@ -3,4 +3,4 @@ import { BigInt } from '@graphprotocol/graph-ts'
 export const BASE_PARAMS = BigInt.fromString('10').pow(9)
 export const BASE_TOKENS = BigInt.fromString('10').pow(18)
 export const MAINTENANCE_MARGIN = BigInt.fromString('62500000')
-export const ROUND_COEFF = BigInt.fromString('3600') // 1 hour
+export const ROUND_COEFF = BigInt.fromString('900') // 1 hour
