ae42c4e148b2ee37a2ea3556302b0506af1bf3b5
Guillaume Nervo <guillaume.nervo@gmail.com>
2021-12-14 18:07:38 +0100

    minor changes to veANGLE

--- angle-subgraph-token/src/mapping/veAngle.ts
+++ angle-subgraph-token/src/mapping/veAngle.ts
@@ -1,7 +1,7 @@
-import { BigInt } from '@graphprotocol/graph-ts'
+import { BigInt, store } from '@graphprotocol/graph-ts'
 import { MAX_LOCK_TIME } from '../../../constants'
 import { veANGLE } from '../../generated/schema'
-import { Deposit } from '../../generated/veAngle/veANGLE'
+import { Deposit, Withdraw } from '../../generated/veAngle/veANGLE'
 
 export function handleDeposit(event: Deposit): void {
   const toId = event.params.provider.toHexString()
@@ -12,7 +12,7 @@ export function handleDeposit(event: Deposit): void {
 
   // deposit / create / extend
   if (type.equals(BigInt.fromString('0')) || type.equals(BigInt.fromString('2'))) {
-    let data = veANGLE.load(toId)
+    let data = veANGLE.load(toId)!
     data.amount = data.amount.plus(amount)
     if (data.locked.ge(timestamp)) {
       data.slope = data.amount.div(MAX_LOCK_TIME)
@@ -32,11 +32,25 @@ export function handleDeposit(event: Deposit): void {
     data.amount = amount
     data.save()
   } else {
-    let data = veANGLE.load(toId)
+    let data = veANGLE.load(toId)!
     data.slope = data.amount.div(MAX_LOCK_TIME)
     data.bias = data.slope.times(locktime.minus(timestamp))
     data.locked = locktime
     data.lastUpdate = timestamp
     data.save()
   }
+}
+
+export function handleWithdraw(event: Withdraw): void {
+  const timestamp = event.params.ts
+  const toId = event.params.provider.toHexString()
+  let data = veANGLE.load(toId)!
+  data.lastUpdate = timestamp
+  data.slope = BigInt.fromString('0')
+  data.bias = BigInt.fromString('0')
+  data.locked = BigInt.fromString('0')
+  data.amount = BigInt.fromString('0')
+  data.save()
+  // or just remove the datapoint
+  // store.remove('veANGLE', toId)
 }
