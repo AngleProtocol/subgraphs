f60fd819a3ad51c98538e0be00d9e133434ff0d6
Jean P <10632523+TamtamHero@users.noreply.github.com>
2022-05-18 19:37:43 +0200

    Add fast sync (#30)

--- constants.ts
+++ constants.ts
@@ -17,3 +17,10 @@ export const MAX_LOCK_TIME = BigInt.fromString('4')
   .times(BigInt.fromString('365'))
   .times(BigInt.fromString('86400'))
 export const WEEK = BigInt.fromString('7').times(BigInt.fromString('86400'))
+// This constant is used to do a fast re-index of the Borrow subgraph after a crash.
+// For events happening below this timestamp, vaults will be updated every (at most) `FAST_SYNC_TIME_INTERVAL` seconds
+// instead of being updated at every oracle update. This is a considerable speedup.
+// Once the subgraph is indexed, this threshold should be set to 0 to launch a slow but accurate indexing in background.
+export const FAST_SYNC_THRESHOLD = BigInt.fromString('0')
+// 3 hours minimal interval between vault refresh (see above)
+export const FAST_SYNC_TIME_INTERVAL = BigInt.fromString('10800')
