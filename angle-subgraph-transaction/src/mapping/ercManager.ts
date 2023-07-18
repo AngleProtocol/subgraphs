14aac5aeb9f8446f6cea7ededbc6cc3fedda0a86
gs8nrv <55771972+GuillaumeNervoXS@users.noreply.github.com>
2023-07-18 16:33:43 +0200

    remove deprecated features (#57)

--- angle-subgraph-transaction/src/mapping/ercManager.ts
+++
@@ -1,6 +0,0 @@
-import { StrategyAdded } from '../../generated/templates/ERCManagerFrontTemplate/PoolManager'
-import { StrategyTemplate } from '../../generated/templates'
-
-export function handleStrategyAdded(event: StrategyAdded): void {
-  StrategyTemplate.create(event.params.strategy)
-}
